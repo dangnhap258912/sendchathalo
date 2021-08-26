818defc30b4265f7ca5bfb5c6939b3e00124c6dd
dangnhap258912 <81761728+dangnhap258912@users.noreply.github.com>
2021-08-26 17:20:50 +0700

    Update sendchat2.php

--- sendchat2.php
+++ sendchat2.php
@@ -95,19 +95,8 @@ if (preg_match('/sex/', $noidung1)||preg_match('/xxx/', $noidung1)||preg_match('
     sendchat($tokenpa,$jsonData);
 }
 
-
- $d = date(d);
-//       if($d <= 22)
-//           {
-//              header("Location:  https://halosession002.herokuapp.com/chatsetss.php?id=$userid&partner=$partner&idpagepa=$idpagepa&idpage=$idpage"); 
-//           }
-//           else
-//           {
-//               header("Location: https://halosession001.herokuapp.com/chatsetss.php?id=$userid&partner=$partner&idpagepa=$idpagepa&idpage=$idpage"); 
-//           }
               header("Location: https://halosession001.herokuapp.com/chatsetss.php?id=$userid&partner=$partner&idpagepa=$idpagepa&idpage=$idpage"); 
 
-  #header("Location: https://halosession002.herokuapp.com/chatsetss.php?id=$userid&partner=$partner&idpagepa=$idpagepa&idpage=$idpage");
 
 
 die();
